573463719b1fa0ff30e0db2afcddef64bcace43c
Urahara-san <yurk282@gmail.com>
2026-06-14 11:25:38 +0300

    Update AXCPT_reactive.js

--- AXCPT_reactive.js
+++ AXCPT_reactive.js
@@ -13,10 +13,8 @@ const { round } = util;
 
 // store info about the experiment session:
 let expName = 'AXCPT_reactive';  // from the Builder filename that created this script
-let expInfo = {
-    'participant': '',
-    'session': '001',
-};
+const urlId = new URLSearchParams(window.location.search).get('id') || '';
+let expInfo = {'participant': urlId, 'session': '001'};
 let PILOTING = util.getUrlParameters().has('__pilotToken');
 
 // Start code blocks for 'Before Experiment'
@@ -28,7 +26,7 @@ const psychoJS = new PsychoJS({
 // open window:
 psychoJS.openWindow({
   fullscr: true,
-  color: new util.Color((0.6549, 0.6549, 0.6549)),
+  color: new util.Color((0.3, 0.3, 0.3)),
   units: 'height',
   waitBlanking: true,
   backgroundImage: '',
